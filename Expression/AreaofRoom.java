ad0e0504a65b8023a73b5d4cacc9d58779a5cb42
madeehaQamar06 <qamarmadeeha3@gmail.com>
2022-12-04 12:19:09 +0530

    AreaofRoom

---
+++ Expression/AreaofRoom.java
@@ -0,0 +1,20 @@
+package project1;
+import java.util.*;
+
+
+public class AreaRoom {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter length and breadth");
+		float l , b ;
+		l = sc.nextFloat();
+		b = sc.nextFloat();
+		float A;
+		A = l * b ;
+		System.out.println("area of room is" + A);
+		
+
+	}
+
+}
