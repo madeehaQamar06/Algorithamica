e9d4c60a3f3cacec792f896470e13375f1c91ca7
madeehaQamar06 <qamarmadeeha3@gmail.com>
2022-12-06 22:44:50 +0530

    Create ColorSquare.java

---
+++ Branching/ColorSquare.java
@@ -0,0 +1,20 @@
+package branching;
+import java.util.*;
+public class ColorSquare {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter input");
+	       String pos;
+		pos = s.next();
+		if(pos.equals("a1")|| pos.equals("c3")|| pos.equals("e5")
+				|| pos.equals("g7")){
+				System.out.println("black square");
+		}
+		else {
+			System.out.println("white square");
+			}
+		}
+			
+
+	}
