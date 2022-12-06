c7c88e0d91ea917ee2e7d73489873aefa6ee7476
madeehaQamar06 <qamarmadeeha3@gmail.com>
2022-12-06 22:35:05 +0530

    Create EvenOdd.java

---
+++ Branching/EvenOdd.java
@@ -0,0 +1,21 @@
+package branching;
+import java.util.*;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter num");
+		int num;
+		num = s.nextInt();
+		if(num %2 == 0) {
+			System.out.println("num is even");
+		}
+		else {
+			System.out.println("num is odd");
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
