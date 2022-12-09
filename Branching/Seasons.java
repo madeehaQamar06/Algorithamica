8ec76a6665d20554470176699e650ed7ff6b7bfe
madeehaQamar06 <qamarmadeeha3@gmail.com>
2022-12-09 14:39:35 +0530

    Update Seasons.java

--- Branching/Seasons.java
+++ Branching/Seasons.java
@@ -10,18 +10,31 @@ public class Seasons {
 		int date;
 		System.out.println("enter date");
 		date = s.nextInt();
-		if(month.equals("march") && date==20) {
-		System.out.println("spring");
+		if(month.equals("january") ||month.equals("february")) {
+		System.out.println("winter");
 		}
-		else if(month.equals("june") && date==21) {
-			System.out.println("summer");
-		}
-		else if(month.equals("sept") && date==22) {
-			System.out.println("fall");
-		}
-		else if(month.equals("dec") && date==21) {
+		else if((month.equals("december") && date>=21) ||(month.equals("march")&& date<=20)){
 			System.out.println("winter");
 		}
+		else if(month.equals("april")||month.equals("may")) {
+			System.out.println("spring");
+		}
+		else if((month.equals("march") && date>20) ||(month.equals("june")&& date<=21)){
+			System.out.println("spring");
+		}
+		else if(month.equals("july")||month.equals("august")) {
+			System.out.println("summer");
+		}
+		else if((month.equals("june") && date>21) ||(month.equals("september")&& date<=22)){
+			System.out.println("summer");
+		}
+		else if(month.equals("october")||month.equals("november")) {
+			System.out.println("fall");
+		}
+		else if((month.equals("september") && date>21) ||(month.equals("december")&& date<=21)){
+			System.out.println("summer");
+		}
+			
 
 	}
 
