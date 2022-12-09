1f82f9cf810132c1eaa84875bf7644ac045350d0
madeehaQamar06 <qamarmadeeha3@gmail.com>
2022-12-09 21:36:16 +0530

    Create sumofn.java

---
+++ repitition/sumofn.java
@@ -0,0 +1,18 @@
+package repitition;
+import java.util.*;
+public class sumofn {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter n");
+		int n = s.nextInt();
+		int sum = 0;
+		for(int i=1;i<=n;++i) {
+			sum = sum + i;
+			
+		}
+		System.out.println("sum is" + sum);
+
+	}
+
+}
