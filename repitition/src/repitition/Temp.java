4235003ef1622d74f228a98974fd6ba11c94c425
madeehaQamar06 <119655423+madeehaQamar06@users.noreply.github.com>
2022-12-12 21:01:22 +0530

    Update Temp.java

--- repitition/src/repitition/Temp.java
+++ repitition/src/repitition/Temp.java
@@ -1,13 +1,16 @@
 package repitition;
+import java.util.Formatter; 
 public class Temp {
 
 	public static void main(String[] args) {
+		Formatter fmt = new Formatter();  
+		fmt.format("%15s %15s \n","celsius" , "fahrenheit" );
 		for(int  c=0;c<=100;c+=10) {
-			int  Fahrenheit =((c*9)/5)+32; 
-			System.out.println("celsius " + " = " + c + "fahrenheit " + " = " + Fahrenheit);
-		
+			int  Fahrenheit =((c*9)/5)+32;
+			
+			fmt.format("%14s %14s \n", c, Fahrenheit);
 		}
+		System.out.println(fmt);
 	}
 }
-
 
